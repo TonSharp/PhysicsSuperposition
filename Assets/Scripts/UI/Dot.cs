5832e3b3f1818ecac9edac15272403c88bec2ab7
TonsLab <labokha.toni@mail.ru>
2021-03-22 23:02:43 +0300

    Line drawing algorithm

--- Assets/Scripts/UI/Dot.cs
+++ Assets/Scripts/UI/Dot.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DotMover : MonoBehaviour
+public class Dot : MonoBehaviour
 {
+    public bool isMain = false;
+    private bool isActive = false;
+
     [SerializeField]
     private float dS;
+    [SerializeField]
+    float width, height;
+    [SerializeField]
+    private DotHolder holder;
     private Vector3 DiffPos;
+    private GameObject Panel;
+    private GameObject panelTemp;
+
+    private void Awake()
+    {
+        Panel = Resources.Load<GameObject>("Prefabs/Setup panel");
+    }
+
+    private void Update()
+    {
+        gameObject.GetComponent<LineRenderer>().SetPosition(0, new Vector3(transform.position.x, transform.position.y, 0));
+        if(isMain) gameObject.GetComponent<LineRenderer>().SetPosition(1, new Vector3(transform.position.x, transform.position.y, 0));
+    }
+
     private void OnMouseEnter() 
     {
         gameObject.GetComponent<Outliner>().enabled = true;
@@ -19,14 +40,37 @@ public class DotMover : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if(!isActive)
+        {
+            isActive = true;
+            panelTemp = Instantiate(Panel, new Vector2(transform.position.x + width, transform.position.y + height), new Quaternion(), GameObject.Find("Canvas").transform);
+            holder.ChangeActiveStates(gameObject);
+
+            panelTemp.GetComponentInChildren<SetupPanel>().parent = gameObject;
+        }
+        else DisableActive();
+
+
+
         Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         DiffPos = new Vector3(MousePos.x - transform.position.x, MousePos.y - transform.position.y, 0);
     }
 
     private void OnMouseDrag() 
     {
-        Debug.Log(DiffPos);
+        if(panelTemp) panelTemp.transform.position = new Vector2(transform.position.x + width, transform.position.y + height);
+
         Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         gameObject.transform.position = new Vector3(MousePos.x - DiffPos.x, MousePos.y - DiffPos.y, 0);
+    }
+    public void DisableActive()
+    {
+        isActive = false;
+
+        Destroy(panelTemp);
+    }
+    public void SetMainValue(bool val)
+    {
+        isMain = val;
     }
 }
